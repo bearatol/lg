7dce99c7b5507ea13cc6b6438ab03651a2749136
Igor Khristenko <ikhristenko@avaya.com>
2021-08-25 18:55:46 +0300

    added closed color

--- lg.go
+++ lg.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/bearatol/lg/color"
 )
 
 type lg struct {
@@ -17,37 +19,37 @@ func loggerCreateNew(std *os.File, color string, prefix string) *lg {
 	}
 }
 
-func (l *lg) print(v ...interface{})   { l.Output(3, fmt.Sprint(v...)) }
-func (l *lg) println(v ...interface{}) { l.Output(3, fmt.Sprintln(v...)) }
+func (l *lg) print(v ...interface{})   { l.Output(3, fmt.Sprint(v...)+color.Color.Reset) }
+func (l *lg) println(v ...interface{}) { l.Output(3, fmt.Sprintln(v...)+color.Color.Reset) }
 func (l *lg) printf(format string, v ...interface{}) {
-	l.Output(3, fmt.Sprintf(format, v...))
+	l.Output(3, fmt.Sprintf(format, v...)+color.Color.Reset)
 }
 
 func (l *lg) fatal(v ...interface{}) {
-	l.Output(3, fmt.Sprint(v...))
+	l.Output(3, fmt.Sprint(v...)+color.Color.Reset)
 	os.Exit(1)
 }
 func (l *lg) fatalln(v ...interface{}) {
-	l.Output(3, fmt.Sprintln(v...))
+	l.Output(3, fmt.Sprintln(v...)+color.Color.Reset)
 	os.Exit(1)
 }
 func (l *lg) fatalf(format string, v ...interface{}) {
-	l.Output(3, fmt.Sprintf(format, v...))
+	l.Output(3, fmt.Sprintf(format, v...)+color.Color.Reset)
 	os.Exit(1)
 }
 
 func (l *lg) panic(v ...interface{}) {
-	s := fmt.Sprint(v...)
+	s := fmt.Sprint(v...) + color.Color.Reset
 	l.Output(3, s)
 	panic(s)
 }
 func (l *lg) panicln(v ...interface{}) {
-	s := fmt.Sprintln(v...)
+	s := fmt.Sprintln(v...) + color.Color.Reset
 	l.Output(3, s)
 	panic(s)
 }
 func (l *lg) panicf(format string, v ...interface{}) {
-	s := fmt.Sprintf(format, v...)
+	s := fmt.Sprintf(format, v...) + color.Color.Reset
 	l.Output(3, s)
 	panic(s)
 }
